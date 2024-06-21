536504e9eae0d5ddae7a6c9b302765f82f1009bb
Rohith1888 <116997360+Rohith1888@users.noreply.github.com>
2024-06-21 16:25:36 +0530

    sone debugging

--- BlogWithAppwrite/src/components/index.js
+++ BlogWithAppwrite/src/components/index.js
@@ -5,7 +5,12 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import Button from "./Button";
 import Input from "./Input";
-
+import Select from "./Select";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./PostForm/PostForm";
+import PostCard from "./PostCard";
 export {
-    Header, Footer,Container, Logo, LogoutBtn, Button, Input
+    Header, Footer,Container, Logo, LogoutBtn, Button, Input, Select, RTE, Signup,Login, PostForm, PostCard
 }
